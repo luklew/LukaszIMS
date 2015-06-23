d34152686f72fb3824af012470571ce6040e8712
unknown <llew@PC-CZC4510050.netbuilder.private>
2015-06-23 14:53:03 +0100

    Added listeners to controller

--- StockManager/src/controller/StockManagerController.java
+++ StockManager/src/controller/StockManagerController.java
@@ -3,6 +3,7 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import view.AddProductFrame;
 import view.MenuBarGUI;
 import view.StockList;
 import view.StockManagerFrame;
@@ -16,22 +17,51 @@ public class StockManagerController {
 	
 	private StockList stockList;
 	private MenuBarGUI menuBar;
+	private AddProductFrame addProduct;
 	
 	public StockManagerController(StockManager model, StockManagerFrame view){
 		this.model = model;
 		this.view = view;
 		
 		stockList = view.getStockList();
+		menuBar = view.getMenuBarGUI();
 		
-		
+		menuBar.addProductListener(new AddProductHandler());
+		menuBar.addSaveToFileListener(new AddSaveToFileHandler());
 		
 		for(Product products : model.getProducts()){
-			stockList.addProductToTable(products.getProductID(), products.getProductName(), products.getProductQuantity());
+			addProductToTable(products.getProductID(), products.getProductName(), products.getProductQuantity());
+		}
+		
+		
+		
+	}
+	
+	public void addProductToTable(String productID, String productName, int productQuantity){
+		stockList.addProductToTable(productID, productName, productQuantity);
+	}
+	
+	public void getLastAndAddProduct(){
+		Product product = model.getLastAddedProduct();
+		addProductToTable(product.getProductID(), product.getProductName(), product.getProductQuantity());
+	}
+	
+	//MENU BAR
+	private class AddProductHandler implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			view.showAddProductFrame();
+			addProduct = view.getProductFrame();
+			
+			addProduct.getProductPanel().addProductListener(new AddProductPanelHandler());
+			addProduct.getProductPanel().addCancelListener(new AddCancelProductHandler());
+			
 		}
 		
 	}
 	
-	private class AddProductHandler implements ActionListener{
+	private class AddSaveToFileHandler implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -46,6 +76,30 @@ public class StockManagerController {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	//ADD PRODUCT FRAME
+	private class AddProductPanelHandler implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			model.addProduct(addProduct.getProductPanel().getProductId(), 
+					addProduct.getProductPanel().getProductName(), 
+					addProduct.getProductPanel().getProductQuantity());
+			
+			getLastAndAddProduct();
+		}
+		
+	}
+	
+	private class AddCancelProductHandler implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			addProduct.dispose();
 			
 		}
 		
