735ae6e43c8b7587af47a94a785549b0fde63f9a
luklew <luklew10@gmail.com>
2015-07-13 02:27:12 +0100

    Color to table rows

--- StockManager/src/view/StockList.java
+++ StockManager/src/view/StockList.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
@@ -11,11 +12,13 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
@@ -26,6 +29,7 @@ import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
+
 
 import model.Product;
 
@@ -62,12 +66,37 @@ public class StockList extends Scene{
         productName.setCellValueFactory(
                 new PropertyValueFactory<Product, String>("productName"));
  
-        TableColumn productQuantity = new TableColumn("Product Quantity");
+        TableColumn<Product, String> productQuantity = new TableColumn("Product Quantity");
         productQuantity.setMinWidth(150);
+        
         productQuantity.setCellValueFactory(
-                new PropertyValueFactory<Product, String>("productQuantity"));
+        		cellData -> cellData.getValue().getProductQuantity());
+        
+        productQuantity.setCellFactory(column -> {
+            return new TableCell<Product, String>() {
+                @Override
+                protected void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+
+                    if (item == null || empty) {
+                        setText(null);
+                        setStyle("");
+                        System.out.println("NULL");
+                    } else {
+
+                        // Style all dates in March with a different color.
+                        if (item.equals(10)) {
+                            System.out.println("10");
+                        } else {
+                            System.out.println("non 10");
+                        }
+                    }
+                }
+            };
+        });
+
  
-        table.setPrefSize(500, 950);
+        table.setPrefSize(500, 700);
         table.setItems(data);
         table.getColumns().addAll(productID, productName, productQuantity);
  
